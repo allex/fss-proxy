320fbdf64dcf495be745e554b0424b39c11b598c
Allex Wang <allex.wxn@gmail.com>
2020-06-03 17:48:21 +0800

    optimize envs

--- Dockerfile
+++ Dockerfile
@@ -6,9 +6,10 @@ LABEL version="${ARG_BUILD_NO}" \
       maintainer="allex_wang <allex.wxn@gmail.com>" \
       description="Base image for FE development integration"
 
-ENV FSS_PROXY=127.0.0.1:8081
+ENV FSS_PROXY=
 ENV FSS_UPSTREAM=
 
+ADD .env /
 ADD ./nginx /etc/nginx
 ADD ./fss-proxy.sh /sbin/
 
--- fss-proxy.sh
+++ fss-proxy.sh
@@ -3,23 +3,21 @@
 
 # ================================================
 # Description: fss-proxy bootstrap
-# Last Modified: Wed Apr 15, 2020 22:55
+# Last Modified: Wed Jun 03, 2020 18:53
 # Author: Allex Wang (allex.wxn@gmail.com)
 # ================================================
 
-echo >&2 "start fss-proxy ..."
-
-export FSS_HOST=${FSS_HOST:-fss.proxy}
-export FSS_PORT=${FSS_PORT:-80}
-export FSS_PROXY=${FSS_PROXY:-127.0.0.1:8081}
-
-if [ -z "$FSS_UPSTREAM" ]; then
-  echo >&2 'fatal: the env "${FSS_UPSTREAM}" is mandatory required'
-  exit 1
+if [ -f /.env ]; then
+  . /.env
 fi
+
+ec=$?
+[ $ec -eq 0 ] || { exit $ec; }
 
 cfile=/etc/nginx/conf.d/default.conf
 vars=`cat "$cfile" |grep '${FSS_.*}' |sed 's/.*\(${FSS_.*}\).*/\1/g'`
+
+echo >&2 "start fss-proxy at port ${FSS_PORT} ..."
 
 envsubst "${vars}" < $cfile > $cfile.tmp \
   && mv $cfile.tmp $cfile \
