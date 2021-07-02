c9f86a3c197719bfdfd697732f797781dd75e57c
Allex Wang <allex.wxn@gmail.com>
2021-07-02 10:21:05 +0800

    feat: pick fss-proxy 1.1.3 configs

--- init.d/sbin/fss-proxy.sh
+++ init.d/sbin/fss-proxy.sh
@@ -3,16 +3,18 @@
 
 # ================================================
 # Description: fss-proxy bootstrap
-# Last Modified: Thu Jan 21, 2021 16:15
+# Last Modified: Thu Jun 24, 2021 20:11
 # Author: Allex Wang (allex.wxn@gmail.com)
 # ================================================
+
+if [ $# -gt 0 ]; then
+  nginx "$@"
+  exit $?
+fi
 
 [ -f /.env ] && { set -a; source /.env; set +a; } || { exit $?; }
 
 echo >&2 "start fss-proxy at port ${FSS_PORT} ..."
-
 ngxfile=/etc/nginx/conf.d/default.conf
-
-envgod < $ngxfile > $ngxfile.tmp \
-  && mv $ngxfile.tmp $ngxfile \
+envgod < $ngxfile > $ngxfile.tmp && mv $ngxfile.tmp $ngxfile \
   && nginx -g 'daemon off;'
